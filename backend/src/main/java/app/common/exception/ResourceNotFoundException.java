55b58a709bedde1811065fac90c9f35f3140d178
Hafidamesad <hafidamesad.acer@gmail.com>
2026-03-10 10:48:59 +0000

    feat: adding property and booking entities

---
+++ backend/src/main/java/app/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package app.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
