208ac4f45729e3f69d19acde5b4b88bac2c6ac12
ousam713 <ousambenz007@gmail.com>
2026-04-07 22:13:21 +0100

    feature : add admin services build : testing docker compose up successfully

--- backend/src/main/java/app/user/admin/entity/Admin.java
+++ backend/src/main/java/app/user/admin/entity/Admin.java
@@ -1,13 +1,15 @@
-package app.user.admin.model;
+package app.user.admin.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
+import lombok.Data;
+import app.user.model.User;
 
+@Data
 @Entity
 @Table(name = "admins")
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public class Admin extends app.user.model.User {
+public class Admin extends User {
     
 }
---
+++ backend/src/main/java/app/user/admin/service/PostService.java
@@ -0,0 +1,5 @@
+package app.user.admin.service;
+
+public class PostService {
+    
+}
