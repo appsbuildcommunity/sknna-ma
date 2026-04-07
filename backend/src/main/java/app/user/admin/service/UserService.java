d51440a450c0a8a2893720fc5f25abc7eb9c1e4b
ousam713 <ousambenz007@gmail.com>
2026-04-07 23:45:36 +0100

    feat : implement admin service

--- backend/src/main/java/app/user/admin/service/UserService.java
+++ backend/src/main/java/app/user/admin/service/UserService.java
@@ -1,5 +1,15 @@
 package app.user.admin.service;
 
+import app.user.admin.dto.AdminRequestDto;
+import app.user.admin.dto.AdminResponseDto;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface UserService {
-    
+    AdminResponseDto create(AdminRequestDto dto);
+    AdminResponseDto getById(UUID id);
+    List<AdminResponseDto> getAll();
+    AdminResponseDto update(UUID id, AdminRequestDto dto);
+    void delete(UUID id);
 }
