4b84adb0201aa1023dfb2f53d5ab0e0d4004f527
ousam713 <ousambenz007@gmail.com>
2026-04-09 15:28:18 +0100

    modify : admin response and request DTOs

---
+++ backend/src/main/java/app/user/admin/dto/AdminUserResponseDTO.java
@@ -0,0 +1,20 @@
+package app.user.admin.dto;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import app.user.model.Role;
+import lombok.Data;
+
+@Data
+public class AdminUserResponseDTO {
+    UUID id;
+    String fullName;
+    String email;
+    String phoneNumber;
+    Role role;
+    boolean isActive;
+    boolean verified;
+    LocalDate createdAt;
+    LocalDate updatedAt;
+}
