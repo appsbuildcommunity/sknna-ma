d001e856684e6784abbee5d8cccd9ae9f88b3bbe
ousam713 <ousambenz007@gmail.com>
2026-04-09 18:23:49 +0100

    modify : comment files : UserService class and its interface

--- backend/src/main/java/app/user/admin/service/UserService.java
+++ backend/src/main/java/app/user/admin/service/UserService.java
@@ -1,15 +1,15 @@
-package app.user.admin.service;
+// package app.user.admin.service;
 
-import app.user.admin.dto.AdminRequestDto;
-import app.user.admin.dto.AdminResponseDto;
+// import app.user.admin.dto.AdminRequestDto;
+// import app.user.admin.dto.AdminResponseDto;
 
-import java.util.List;
-import java.util.UUID;
+// import java.util.List;
+// import java.util.UUID;
 
-public interface UserService {
-    AdminResponseDto create(AdminRequestDto dto);
-    AdminResponseDto getById(UUID id);
-    List<AdminResponseDto> getAll();
-    AdminResponseDto update(UUID id, AdminRequestDto dto);
-    void delete(UUID id);
-}
+// public interface UserService {
+//     AdminResponseDto create(AdminRequestDto dto);
+//     AdminResponseDto getById(UUID id);
+//     List<AdminResponseDto> getAll();
+//     AdminResponseDto update(UUID id, AdminRequestDto dto);
+//     void delete(UUID id);
+// }
